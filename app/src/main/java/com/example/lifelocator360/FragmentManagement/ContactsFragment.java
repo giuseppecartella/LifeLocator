fc61fe7509bf9a1da47db86af56aeb591bb868ec
Cartella Giuseppe <cartellagiuseppe@hotmail.it>
2019-07-30 09:12:11 +0200

    Aggiornamento mattina

--- app/src/main/java/com/example/lifelocator360/FragmentManagement/ContactsFragment.java
+++ app/src/main/java/com/example/lifelocator360/FragmentManagement/ContactsFragment.java
@@ -57,6 +57,8 @@ public class ContactsFragment extends Fragment implements View.OnClickListener {
 
     }
 
+
+    
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
