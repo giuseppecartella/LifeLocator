216cf1f39ec5732981a60ff70dd643b5a273a35c
Giuseppe <cartellagiuseppe@hotmail.it>
2019-09-12 22:45:50 +0200

    terminazione logo app

--- app/src/main/java/com/example/lifelocator360/SplashScreenManagement/SplashActivity.java
+++ app/src/main/java/com/example/lifelocator360/SplashScreenManagement/SplashActivity.java
@@ -392,7 +392,6 @@ public class SplashActivity extends AppCompatActivity {
     }
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
